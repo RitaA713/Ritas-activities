d2b91c79ae7a36f237bcf8d996944c46190e2bc2
Rita <Rita@DESKTOP-13L6P0J.Home>
2024-05-02 20:30:44 +0100

    test1

---
+++ Test Suites/ts1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2decbdc7-3383-40db-83ba-6c5119b89e4c</testSuiteGuid>
+</TestSuiteEntity>
